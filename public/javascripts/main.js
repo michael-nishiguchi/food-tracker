3d88076b2623dcfc6fe57bc45f5dd0576ed49229
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-01-24 09:16:19 -0500

    google

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,7 +7,18 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
-    getBasicProfile();
+
+    if(auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    }
+    GoogleUser.getBasicProfile();
+
     function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
