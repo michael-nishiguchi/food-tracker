8f0a5859a2cadc1216e53c1c05c7b6dab612439d
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-01-24 02:14:07 -0500

    google test

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,5 +6,13 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
   
+        getBasicProfile();
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+          }
     });
   });
