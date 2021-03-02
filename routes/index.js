b13f3cc0ee19ab9a6e9b01a5de4fc85f1d30dca7
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-03-02 13:37:23 -0500

    npm google

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Food Tracker' });
 });
 router.post('/verifyToken', function(req, res, next) {
-  return res.redirect("https://facebook.com");
+  const client = new OAuth2Client(CLIENT_ID);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+  }
+  verify().catch(console.error);
+  res.render('index', {
+    message: "success"
+  });
 });
 module.exports = router;
