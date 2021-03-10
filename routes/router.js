55a834995f30ccb3780476cbb92dbf73575648e9
nishiguchi-byui <43350657+nishiguchi-byui@users.noreply.github.com>
2021-03-10 13:22:08 -0500

    log google credentials

--- routes/router.js
+++ routes/router.js
@@ -10,6 +10,10 @@ const app_key = process.env.APPLICATION_KEY;
 const fetch = require('node-fetch');
 const { render } = require('../app');
 
+console.log('client id: ' + client);
+console.log('app id: ' + app_id);
+console.log('app key: ' + app_key);
+
 //database
 const connectionString = process.env.DATABASE_URL;
 const pg = require('pg');
